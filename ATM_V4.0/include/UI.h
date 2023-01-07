8f489b47728456a6f0ca10636bb329f30469a50e
Flash-Z <65103912+Flash-Z@users.noreply.github.com>
2023-01-07 11:44:14 +0800

    Add files via upload
    Probably finnal version

---
+++ ATM_V4.0/include/UI.h
@@ -0,0 +1,38 @@
+#ifndef UI_H
+#define UI_H
+
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+#include "UI_insert_card.h"
+#include "UI_inquiry.h"
+#include "UI_menu.h"
+#include "colorSet.h"
+#include "UI_withdraw.h"
+#include "UI_Transfer.h"
+#include "UI_changePSW.h"
+#include "UI_deposit.h"
+
+using namespace std;
+
+class UI
+{
+public:
+    // 创建子UI实例
+    UI_welcome welcome;
+    UI_inquiry inquiry;
+    UI_menu menu;
+    UI_withdraw withdraw;
+    UI_transfer transfer;
+    UI_changePSW changePSW;
+    UI_deposit deposit;
+
+public:
+    // 返回功能
+    void back();
+    // 文件打开失败报错
+    void file_open_failure();
+};
+
+#endif
